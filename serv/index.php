01121b190b397a186193a2c17abdfea4f5c8583f
Scelesto <Scelesto@users.noreply.github.com>
2015-04-20 11:18:14 -0400

    Added file loader

--- serv/index.php
+++ serv/index.php
@@ -6,5 +6,8 @@ Park School of Baltimore Senior Project 2015
 Started v0 4/20/2015
 */
 include("rootfuncs.php"); //Consts ROOT,SERV,CALL,NL; Funcs prnt,err,getClass
-
+include("pageref.php"); //Reference for URI to file conversion
+if(array_key_exists(CALL,$pages)){
+	include('pages/'.$pages[CALL]); //Load correct file
+}
 ?>
---
+++ serv/pageref.php
@@ -0,0 +1,5 @@
+<?php
+$pages=array(
+	""=>"home.php"
+);
+?>
