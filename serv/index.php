992eabcdf5391cf853971de3586c59cb34360a3a
Scelesto <Scelesto@users.noreply.github.com>
2015-04-20 11:03:41 -0400

    Removed redundant variable

--- serv/index.php
+++ serv/index.php
@@ -5,7 +5,6 @@ by Abram Foster
 Park School of Baltimore Senior Project 2015
 Started v0 4/20/2015
 */
-$data=$_REQUEST;
 include("rootfuncs.php"); //Consts ROOT,SERV,CALL,NL; Funcs prnt,err,getClass
 
 ?>
